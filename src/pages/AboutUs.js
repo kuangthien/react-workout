a4260b50d86684f39dde85e57cdd492a1af48b0e
Tran Kuang Thien <kuangthien@gmail.com>
2018-12-11 20:37:32 +0700

    remove unncessary ppackge

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,20 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class AboutUs extends Component {
     render() {
         return (
             <div className="container">
-            <div className="col-8">
-            
-            <h1>
-                About Us
-            </h1>
-            <div className="content">
-            
-            Content About Us
+                <h1>About Us</h1>
+                <div className="content">Content About Us</div>
             </div>
-            </div>
-            </div>
-        )
+        );
     }
 }
